18a14e628f7cbcc301f5da6a8d0bca62332e3da3
annemvikramreddy <annemvikramreddy@gmail.com>
2024-02-08 23:19:56 -0500

    started learning bascis of nextjs

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import AddToCart from './AddToCart'
+// import styles from './ProductCard.module.css'
+
+
+
+
+const ProductCard = () => {
+  return (
+    <div >
+        {/* <button onClick={()=>console.log("click")}>Add to Cart</button> */}
+        <AddToCart/>
+    </div>
+  )
+}
+
+export default ProductCard
